6c3ae9b9530dba6dca82064bd4f44814aee5463d
laurindocbenjamim <rocketmc2009@gmail.com>
2025-04-01 01:27:06 +0100

    add cors origins

--- app/utils/extentions.py
+++ app/utils/extentions.py
@@ -36,6 +36,7 @@ def load_extentions(*, app):
                     r"/protected": {"origins": cors_origin},
                     r"/logout-with-revoking-token": {"origins": cors_origin},
                     r"/api/v1/web-scrapping/*": {"origins": cors_origin},
+                    r"/api/v1/speech_recognition/*": {"origins": cors_origin},
     })
     cors_p.init_app(app)
     limiter.init_app(app)     
